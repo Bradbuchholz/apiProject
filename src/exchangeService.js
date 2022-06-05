87fdb7a20112688a48d6e6a64a08eaf867ea0bab
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-04 17:46:53 -0700

    update exchangeService.js

--- src/exchangeService.js
+++ src/exchangeService.js
@@ -1,5 +1,24 @@
-export default class exchangeService {
-  static getRate() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+import { ResolvePlugin } from "webpack";
+
+export default class exchangeService {  
+  static getRate(currency) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
+
+
+
+
+
