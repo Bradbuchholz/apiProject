6cab43684bc297fcc152caa31ef37f150a31fbce
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-04 18:02:19 -0700

    update exchange.js

--- src/exchange.js
+++ src/exchange.js
@@ -1,4 +1,10 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+export class exchanger {
+  constructor(inputCurrency, inputAmount, exchangeRateObject) {
+    this.USD = exchangeRateObject.converson_rates.USD,
+    this.EUR = exchangeRateObject.converson_rates.EUR,
+    this.CHF = exchangeRateObject.converson_rates.CHF,
+    this.AED = exchangeRateObject.converson_rates.AED,
+    this.AMD = exchangeRateObject.converson_rates.AMD,
+    this.BBD = exchangeRateObject.converson_rates.BBD,
+  }
+}
