2621350f1170212eab028662f421593c705c42de
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-04 18:30:47 -0700

    dinner break

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import exchangeService from './exchangeService.js';
+import exchanger from './exchange.js';
