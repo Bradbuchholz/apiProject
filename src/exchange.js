e7dee6c33d8379221d5f0444f76a74acb48b7e46
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-04 18:10:16 -0700

    update exchange.js

--- src/exchange.js
+++ src/exchange.js
@@ -6,5 +6,15 @@ export class exchanger {
     this.AED = exchangeRateObject.converson_rates.AED,
     this.AMD = exchangeRateObject.converson_rates.AMD,
     this.BBD = exchangeRateObject.converson_rates.BBD,
+    this.CUP = exchangeRateObject.converson_rates.CUP,
+    this.inputCurrency = inputCurrency,
+    this.inputAmount = inputAmount,
+    this.outPutAmount = this.exchangeResults(this.inputCurrency,this.inputAmount)
+  }
+
+  exchangeResults(inputCurrency,inputAmount) {
+    let exchangeValue = this[inputCurrency];
+    let result = exchangeValue*inputAmount;
+    return result;
   }
 }
