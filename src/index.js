239862222bc7c43f5cb96181e83ac4ca1e2fa029
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-04 23:23:26 -0700

    update index.js, currently no output showing

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@ import './css/styles.css';
 import exchangeService from './exchangeService.js';
 import exchanger from './exchange.js';
 
+function clearFields() {
+  $('.amount').val("");
+  $('.showError').text("");
+}
+
+$(document).ready(function() {
+  $('#exchangeSubmit').click(function() {
+    let curr = $('#exchangeCurrency').val();
+    let promie = exchangeService.getRate(currency);
+    Promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.amount').text('This is the value of your exchange ${curr}');
+    }, function(error) {
+      $('.showError').text('There was an error processing this request');
+    });
+  });
+});
