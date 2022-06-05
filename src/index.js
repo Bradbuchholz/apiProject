f49426fee06dd55569d909e0e3d3c46d1b6200ca
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-04 23:50:30 -0700

    update index.js, currently no output showing

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ function clearFields() {
 $(document).ready(function() {
   $('#exchangeSubmit').click(function() {
     let curr = $('#exchangeCurrency').val();
-    let promie = exchangeService.getRate(currency);
+    let promise = exchangeService.getRate(currency);
     Promise.then(function(response) {
       const body = JSON.parse(response);
-      $('.amount').text('This is the value of your exchange ${curr}');
+      $('.amount').text(`This is the value of your exchange ${curr}`);
     }, function(error) {
       $('.showError').text('There was an error processing this request');
     });
