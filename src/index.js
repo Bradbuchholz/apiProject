7b12813864cdeed4d83fac41a9be1702423d8cc2
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-07 15:25:51 -0700

    taking a break

--- src/index.js
+++ src/index.js
@@ -4,17 +4,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './js/exchangeService.js';
 
-function displayExchange(response, usDollar, currency) {
-  if (response.result != 'sucess') {
-    $('#displayExchange').text(`An error has occured: ${response.message}`);
-  } else if (!currency) {
-    $('#displayExchange').text('Please select a currency');
-  } else if (usDollar === '') {
-    $('#displayExchange').text('Please select the amount in USD');
-  } else if (response.result === 'sucess') {
-    $('#displayExchange').text(`${usDollar} in ${currency}: ${response.conversion_rates[currency]*usDollar}`);
-  } else {
-    $('#displayExchange').text('There was an error');
-  }
-}
+
 
