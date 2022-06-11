12c49a534528b8aac44a24b5ac949962a3cac19a
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-11 14:48:56 -0700

    update exchangeService.js and create .babelrc

--- src/js/exchangeService.js
+++ src/js/exchangeService.js
@@ -1,15 +1,14 @@
 export default class ExchangeService {
-  static async getExchange() {
-    try {
-      const rateResponse = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-      if (!rateResponse.ok) {
-        throw Error(rateResponse.statusText);
-      }
-      return await rateResponse.json();
-    } catch (error) {
-      return error;
-    }
+  static getExchange(targetCode, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCode}/${amount}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
-
-
