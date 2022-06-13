cc378fab8375046817db423874365d278b7cd4a3
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-13 16:23:15 -0700

    change exchangeService.js to promise

--- src/js/exchangeService.js
+++ src/js/exchangeService.js
@@ -1,14 +1,17 @@
 export class ExchangeService {
-  static getExchange(currency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      .then(function(response) {
-        if(!response.ok) {
-          throw Error(response.statusText);
+  static getRate() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
         }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      });
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
